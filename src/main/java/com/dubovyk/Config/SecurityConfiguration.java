08a68c4da92a8e0670f2c6fb03ada8447284e7b4
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2017-05-09 14:29:00 +0300

    Added authorization service provider and updated UserDAO.

---
+++ src/main/java/com/dubovyk/Config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.dubovyk.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ * This is a settings class for security.
+ *
+ * It`s done as a Java code due to some
+ * problems building xml beans (don`t work at all).
+ *
+ * @author Sergey Dubovyk aka knidarkness
+ * @version 1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final CustomAuthenticationProvider authProvider;
+
+    @Autowired
+    public SecurityConfiguration(CustomAuthenticationProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider);
+        //auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/all").access("hasRole('USER')")
+                .antMatchers("/user/add").access("hasRole('ADMIN')")
+                .and().httpBasic();
+    }
+}
